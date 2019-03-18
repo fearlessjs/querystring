3ee4cbf2d6375ce89fb4549c884123bb7c94a0b8
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2019-03-17 23:41:27 -0300

    improve index

---
+++ index.js
@@ -0,0 +1,17 @@
+const { parse } = require('querystringify')
+
+const queryString = (req, res) => {
+  req.query = parse(req.getQuery())
+}
+
+module.exports = options => {
+  return Array.isArray(options)
+    ? {
+      handlers: options,
+      middlewares: [queryString]
+    }
+    : {
+      ...options,
+      middlewares: [...options.middleware, queryString]
+    }
+}
